ddaa06384f51f7a84f1b59ae8e2e078e5d2b15a2
thodkatz <thodkatz@gmail.com>
2020-11-15 17:21:33 +0200

    Naive approach without duplicate triangles

--- v1.py
+++ v1.py
@@ -1,24 +1,25 @@
-# adjacency matrix is symmetrical because the graph that represents is undirected.
-# So we know that we want to iterate only the one half of the array. Are you sure?
-# If there is a triangle for the edge 1-2, then the node 1 and 2 has one triangle 
-# as centered nodes of a triplet. Actually for that node there are two triangles,
-# if you swap the other two element
-def count_triangles (adjacency):
-    length = len(adjacency)
-    count = [[0] * length] 
+def countTriangles (adjacency):
+    length = len(adjacency[0])
+    vertices = [0] * length 
 
     for i in range(length):
-        for j in range(i, length):
-            if (i == j): 
-                continue
+        for j in range(i+1, length): # --> for j in range(length) will result to take into acvertices 3 times one triangle
             if (adjacency[i][j]==1):
-                for k in range (0,length):
-                    if (k == j):
-                        continue
-                    if (adjacency[j][k] == 1):
-                        if (adjacency[k][i] == 1):
-                            count[i]+=1
-                            count[j]+=1
-                            count[k]+=1
+                for k in range(j+1,length): # --> for k in range(length) will result to take into acvertices 2 times one triangle
+                    if (adjacency[j][k] == 1 and adjacency[k][i] == 1):
+                        vertices[i] +=1
+                        vertices[k] +=1
+                        vertices[j] +=1
+    return vertices
 
 
+# Driver code
+graph = [[0, 1, 1, 0],  
+         [1, 0, 1, 1],  
+         [1, 1, 0, 1],  
+         [0, 1, 1, 0]] 
+  
+print("How many times a vertix belong to a triangle?") 
+vertices = countTriangles(graph)
+for i in range(len(graph)):
+    print("The vertice: " + str(i) + " belong to " + str(vertices[i]) + " triangles")
